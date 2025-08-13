06c855ab1f189f8e1df47a60de5eef341cfbaccc
qjwh <121411508+qjwh@users.noreply.github.com>
2025-08-13 09:46:48 +0800

    Update load.js

--- resources/load.js
+++ resources/load.js
@@ -17,7 +17,7 @@ const resources = [
   { type: 'script', url: '../files/index.umd.js' },
   
   // 本地资源（放在最后）
-  { type: 'link', url: 'main.css' },
+  // { type: 'link', url: 'main.css' },
   { type: 'script', url: 'main.js' }
 ];
 
